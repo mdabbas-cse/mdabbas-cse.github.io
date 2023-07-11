20cd36c1f82d40142d4ef05cae264c6985f6cfb9
mdabbas-cse <gmabbas44@gmail.com>
2023-07-11 12:41:03 +0600

    update: js minfy, change profile image, remove image grayscale style

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+    // document.querySelector(".theme-btn").addEventListener("click", () => {
+    //     document.body.classList.toggle("light-mode");
+    // })
 })();
